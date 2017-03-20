6b5c5ffbb16b656fe0fffc8999ba052559ecaec4
Geo Calancea <geo.calancea@gmail.com>
2017-03-20 11:20:41 +0200

    New change to Client

--- Client.java
+++ Client.java
@@ -4,16 +4,16 @@
  * and open the template in the editor.
  */
 package ip.lab5;
+
 import java.util.Vector;
 
 /**
- *
  * @author dell
  */
 
 
 public class Client {
- 
+
     public String nume;
 
     public Integer id;
@@ -21,26 +21,31 @@ public class Client {
     public String telefon;
 
     public String email;
-    
-    public Client(String name, int id, String telefon, String email){
-    this.id=id;
-    this.nume=name;
-    this.telefon=telefon;
-    this.email=email;
-    };
-  
 
-   public Vector  myRezervare;
-    public Vector  myManager_Rezervari;
+    public Client(String name, int id, String telefon, String email) {
+        this.id = id;
+        this.nume = name;
+        this.telefon = telefon;
+        this.email = email;
+    }
+
+    ;
+
+    public void afis() {
+        System.out.println("Numele clientului: " + this.nume);
+    }
+
+    public Vector myRezervare;
+    public Vector myManager_Rezervari;
 
     public void rezerva() {
-  }
+    }
 
-  public void anuleaza(Rezervare rezervare) {
-  }
+    public void anuleaza(Rezervare rezervare) {
+    }
 
     public Vector<Rezervare> rezervari() {
-  return null;
-  }
+        return null;
+    }
 
 }
