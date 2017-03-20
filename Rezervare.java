6b808a584935381f0bfe20c188c73717b79884b4
Juve45 <juve45@rocketmail.com>
2017-03-20 11:04:06 +0200

    upd rezervare

--- Rezervare.java
+++ Rezervare.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ip.lab5;
-
-/**
- *
- * @author dell
- */
+import java.util.Vector;
 
 public class Rezervare {
 
@@ -19,6 +9,9 @@ public class Rezervare {
   public Integer nrMese;
 
   public void Rezervare(Client client, Restaurant restaurant, Integer nrMese) {
+  	this.client = client;
+  	this.restaurant = restaurant;
+  	this-nrMese = nrMese;
   }
 
 }
