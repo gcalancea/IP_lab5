e929b8f1689feb27a0a34ca0a3e8a84cf9f40628
Turcu Nicusor <nicusor.turcu@info.uaic.ro>
2017-03-20 11:04:36 +0200

    manager_rezervari

---
+++ ManagerRezervari.java
@@ -0,0 +1,22 @@
+/**
+ * Project name Laborator5java.
+ * Created by Turcu Nicusor on 20-Mar-17.
+ */
+import java.util.Vector;
+
+public class ManagerRezervari {
+    private Vector <Rezervare> rezervari;
+
+    public Boolean adaugaRezervare(Integer idClient) {
+
+    }
+    public void stergeRezervare(Client client) {
+
+    }
+    public Boolean isAvailable(Rezervare rezervare) {
+
+    }
+    public Vector<Rezervare> getRezervari(Client client) {
+
+    }
+}
