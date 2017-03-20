e94a2052be734871d13804561d9c9748fc669430
Djok216 <motroi.valeriu@yahoo.com>
2017-03-20 11:31:07 +0200

    final Manager Rezervari

--- src/ManagerRezervari.java
+++ src/ManagerRezervari.java
@@ -19,16 +19,38 @@ public class ManagerRezervari {
         return instance;
     }
 
-    public Boolean adaugaRezervare(Integer idClient) {
-        return true;
+    public Boolean adaugaRezervare(Rezervare rez) {
+        if(isAvailable(rez)) {
+            rezervari.add(rez);
+            return true;
+        }
+        return false;
     }
-    public void stergeRezervare(Client client) {
-        
+
+    public void stergeRezervare(Rezervare rez) {
+        for(Rezervare now : rezervari)
+            if(now.equals(rez)) {
+                rezervari.remove(now);
+                System.out.println("Rezervarea a fost stearsa!");
+                return ;
+            }
+        System.out.println("Rezervarea nu a fost gasita!");
     }
+
     public Boolean isAvailable(Rezervare rezervare) {
-        return true;
+        int cnt = rezervare.getRestaurant().getNrMese();
+        String name = rezervare.getRestaurant().getName().lower();
+        for(Rezervare now : rezervari)
+            if(rezervare.getRestaurant().getName().lower().equals(name))
+                cnt -= rezervare.getNrMese();
+
+        return cnt >= rezervare.getNrMese() && cnt > 0;
     }
     public Vector<Rezervare> getRezervari(Client client) {
-        return new Vector<Rezervare>();
+        Vector<Rezervare> ans = new Vector<>();
+        for(Rezervare now : rezervari)
+            ans.add(now);
+
+        return ans;
     }
 }
